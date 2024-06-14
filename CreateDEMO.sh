b16f43c846196f9dfe3b0fdef388d2776edc18d8
GrayNskDEMO <161566997+GrayNskDEMO@users.noreply.github.com>
2024-06-14 21:34:49 +0700

    Update CreateDEMO.sh

--- CreateDEMO.sh
+++ CreateDEMO.sh
@@ -14,23 +14,23 @@ read -p "Enter the local storage name: " STORAGE
 curl -L $(yadisk-direct https://disk.yandex.ru/d/cX4HEQS4_VXOTw) -o ISP-disk001.vmdk
 curl -L $(yadisk-direct https://disk.yandex.ru/d/1_vGyrhtlGtQpw) -o HQ-SRV-disk001.vmdk
 curl -L $(yadisk-direct https://disk.yandex.ru/d/RKc3dBcAuFQ4tg) -o CLI-disk001.vmdk
-qm create 101 --name "ISP" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr1 --net0 virtio,bridge=vmbr1 --net0 virtio,bridge=vmbr1
+qm create 101 --name "ISP" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr1 --net0 virtio,bridge=vmbr11 --net0 virtio,bridge=vmbr12
 qm importdisk 101 ISP-disk001.vmdk $STORAGE --format qcow2
 qm set 101 -sata0 $STORAGE:101/vm-101-disk-0.qcow2 --boot order=ide0
 echo "ISP is done!!!"
-qm create 102 --name "HQ-R" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
+qm create 102 --name "HQ-R" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
 qm importdisk 102 ISP-disk001.vmdk $STORAGE --format qcow2
 qm set 102 -sata0 $STORAGE:102/vm-102-disk-0.qcow2 --boot order=ide0
-echo "ISP is done!!!"
-qm create 101 --name "ISP" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
+echo "HQ-R is done!!!"
+qm create 103 --name "BR-R" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
+qm importdisk 101 ISP-disk001.vmdk $STORAGE --format qcow2
+qm set 101 -ide0 $STORAGE:101/vm-101-disk-0.qcow2 --boot order=ide0
+echo "BR-R is done!!!"
+qm create 104 --name "HQ-SRV" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
 qm importdisk 101 ISP-disk001.vmdk $STORAGE --format qcow2
 qm set 101 -ide0 $STORAGE:101/vm-101-disk-0.qcow2 --boot order=ide0
 echo "ISP is done!!!"
-qm create 101 --name "ISP" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
-qm importdisk 101 ISP-disk001.vmdk $STORAGE --format qcow2
-qm set 101 -ide0 $STORAGE:101/vm-101-disk-0.qcow2 --boot order=ide0
-echo "ISP is done!!!"
-qm create 101 --name "ISP" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
+qm create 101 --name "BR-SRV" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr7
 qm importdisk 101 ISP-disk001.vmdk $STORAGE --format qcow2
 qm set 101 -ide0 $STORAGE:101/vm-101-disk-0.qcow2 --boot order=ide0
 echo "ISP is done!!!"
