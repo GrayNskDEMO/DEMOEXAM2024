cb29316456cd9a46025791b5c937205970663601
GrayNskDEMO <161566997+GrayNskDEMO@users.noreply.github.com>
2024-06-14 23:12:30 +0700

    Update ADDDemo.sh

--- ADDDemo.sh
+++ ADDDemo.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-cat ./BridgeInterfaces.txt >> /etc/network/interfaces;
-for s in $(seq 1 10); do
-for i in $(seq 1 4); do
-mkdir /etc/net/ifaces/vmbr$s$i ; cp ./vmbropt.txt /etc/net/ifaces/vmbr$s$i/options
-done
-done
-systemctl restart network;
-apt-get install python3-pip python3-venv -y;
-python3 -m venv myenv;
-source myenv/bin/activate;
-pip3 install wldhx.yadisk-direct;
-read -p "Enter the local storage name: " STORAGE
-curl -L $(yadisk-direct https://disk.yandex.ru/d/cX4HEQS4_VXOTw) -o ISP-disk001.vmdk
-curl -L $(yadisk-direct https://disk.yandex.ru/d/1_vGyrhtlGtQpw) -o HQ-SRV-disk001.vmdk
-curl -L $(yadisk-direct https://disk.yandex.ru/d/RKc3dBcAuFQ4tg) -o CLI-disk001.vmdk
 for s in $(seq 1 10); do
 qm create 101 --name "ISP" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr1 --net1 virtio,bridge=vmbr11 --net2 virtio,bridge=vmbr12
 qm importdisk 101 ISP-disk001.vmdk $STORAGE --format qcow2
