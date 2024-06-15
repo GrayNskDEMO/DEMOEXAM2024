c3f0c8f36d0503da4a70085bf5adc2cd35480cd3
GrayNskDEMO <161566997+GrayNskDEMO@users.noreply.github.com>
2024-06-15 16:03:02 +0700

    Update CreateDEMO.sh

--- CreateDEMO.sh
+++ CreateDEMO.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cat ./BridgeInterfaces.txt >> /etc/network/interfaces;
+mkdir /etc/net/ifaces/vmbr1 ; cp ./vmbropt.txt /etc/net/ifaces/vmbr1/options
 for s in $(seq 1 10); do
 for i in $(seq 1 4); do
 mkdir /etc/net/ifaces/vmbr$s$i ; cp ./vmbropt.txt /etc/net/ifaces/vmbr$s$i/options
@@ -15,29 +16,32 @@ curl -L $(yadisk-direct https://disk.yandex.ru/d/cX4HEQS4_VXOTw) -o ISP-disk001.
 curl -L $(yadisk-direct https://disk.yandex.ru/d/1_vGyrhtlGtQpw) -o HQ-SRV-disk001.vmdk
 curl -L $(yadisk-direct https://disk.yandex.ru/d/RKc3dBcAuFQ4tg) -o CLI-disk001.vmdk
 for s in $(seq 1 10); do
-qm create 101 --name "ISP" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr1 --net1 virtio,bridge=vmbr11 --net2 virtio,bridge=vmbr12
-qm importdisk 101 ISP-disk001.vmdk $STORAGE --format qcow2
-qm set 101 -sata0 $STORAGE:101/vm-101-disk-0.qcow2 --boot order=sata0
+let s1=$s+10
+Vm=(${s1}1)
+IntNum=vmbr$s
+qm create ${Vm}1 --name "ISP" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr1 --net1 virtio,bridge=${IntNum}1 --net2 virtio,bridge=${IntNum}2
+qm importdisk ${Vm}1 ISP-disk001.vmdk $STORAGE --format qcow2
+qm set ${Vm}1 -sata0 $STORAGE:${Vm}1/vm-${Vm}1-disk-0.qcow2 --boot order=sata0
 echo "ISP"$s" is done!!!"
-qm create 102 --name "HQ-R" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr11 --net1 virtio,bridge=vmbr13  
-qm importdisk 102 ISP-disk001.vmdk $STORAGE --format qcow2
-qm set 102 -sata0 $STORAGE:102/vm-102-disk-0.qcow2 --boot order=sata0
+qm create ${Vm}2 --name "HQ-R" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=${IntNum}1 --net1 virtio,bridge=${IntNum}3  
+qm importdisk ${Vm}2 ISP-disk001.vmdk $STORAGE --format qcow2
+qm set ${Vm}2 -sata0 $STORAGE:${Vm}2/vm-${Vm}2-disk-0.qcow2 --boot order=sata0
 echo "HQ-R"$s" is done!!!"
-qm create 103 --name "BR-R" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr12 --net1 virtio,bridge=vmbr14
-qm importdisk 103 ISP-disk001.vmdk $STORAGE --format qcow2
-qm set 103 -sata0 $STORAGE:103/vm-103-disk-0.qcow2 --boot order=sata0
+qm create ${Vm}3 --name "BR-R" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=${IntNum}2 --net1 virtio,bridge=${IntNum}4
+qm importdisk ${Vm}3 ISP-disk001.vmdk $STORAGE --format qcow2
+qm set ${Vm}3 -sata0 $STORAGE:${Vm}3/vm-${Vm}3-disk-0.qcow2 --boot order=sata0
 echo "BR-R"$s" is done!!!"
-qm create 104 --name "HQ-SRV" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr13
-qm importdisk 104 HQ-SRV-disk001.vmdk $STORAGE --format qcow2
-qm set 104 -sata0 $STORAGE:104/vm-104-disk-0.qcow2 --boot order=sata0
+qm create ${Vm}4 --name "HQ-SRV" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=${IntNum}3
+qm importdisk ${Vm}4 HQ-SRV-disk001.vmdk $STORAGE --format qcow2
+qm set ${Vm}4 -sata0 $STORAGE:${Vm}4/vm-${Vm}4-disk-0.qcow2 --boot order=sata0
 echo "HQ-SRV"$s" is done!!!"
-qm create 105 --name "BR-SRV" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr14
-qm importdisk 105 HQ-SRV-disk001.vmdk $STORAGE --format qcow2
-qm set 105 -sata0 $STORAGE:105/vm-105-disk-0.qcow2 --boot order=sata0
+qm create ${Vm}5 --name "BR-SRV" --cores 2 --memory 2048 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=${IntNum}4
+qm importdisk ${Vm}5 HQ-SRV-disk001.vmdk $STORAGE --format qcow2
+qm set ${Vm}5 -sata0 $STORAGE:${Vm}5/vm-${Vm}5-disk-0.qcow2 --boot order=sata0
 echo "BR-SRV"$s" is done!!!"
-qm create 106 --name "CLI" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr1
-qm importdisk 106 CLI-disk001.vmdk $STORAGE --format qcow2
-qm set 106 -sata0 $STORAGE:106/vm-106-disk-0.qcow2 --boot order=sata0
+qm create ${Vm}6 --name "CLI" --cores 2 --memory 4096 --ostype l26 --scsihw virtio-scsi-single  --net0 virtio,bridge=vmbr1
+qm importdisk ${Vm}6 CLI-disk001.vmdk $STORAGE --format qcow2
+qm set ${Vm}6 -sata0 $STORAGE:${Vm}6/vm-${Vm}6-disk-0.qcow2 --boot order=sata0
 echo "CLI"$s" is done!!!"
 done
 echo "ALL DONE!!!"
