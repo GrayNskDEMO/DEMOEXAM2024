3d43c68ed101478dd02d861c487f4f318c7e318f
GrayNskDEMO <161566997+GrayNskDEMO@users.noreply.github.com>
2024-06-16 18:02:33 +0700

    Update CreateInterface.sh

--- CreateInterface.sh
+++ CreateInterface.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 #Создание интерфейса для машины ISP0 -доступ в интернет для всех стендов
 #cat ./BridgeInterfaces.txt >> /etc/network/interfaces.new;
-echo auto vmbr1 >> /etc/network/interfaces.new1;
-echo iface vmbr1 inet manual >> /etc/network/interfaces.new;
-echo \t\tbridge-ports none >> /etc/network/interfaces.new;
-echo "         bridge-stp off" >> /etc/network/interfaces.new;
-echo "         bridge-fd 0" >> /etc/network/interfaces.new;
-echo \#ISP0 >> /etc/network/interfaces.new;
+FiileInt=/etc/network/interfaces.new1
+echo auto vmbr1 >> FiileInt;
+echo iface vmbr1 inet manual >> FiileInt;
+echo -e '\t'bridge-ports none >> FiileInt;
+echo -e '\t'bridge-stp off >> FiileInt;
+echo -e '\t'bridge-fd 0 >> FiileInt;
+echo \#ISP0 >> FiileInt;
 #Создание интерфейсов для виртуальных машин
 for s in $(seq 1 20); do
 for i in $(seq 1 4); do
 IntNum=vmbr$s$i
-echo auto ${IntNum} >> /etc/network/interfaces.new;
-echo iface ${IntNum} inet manual >> /etc/network/interfaces.new;
-echo "         bridge-ports none" >> /etc/network/interfaces.new;
-echo "         bridge-stp off" >> /etc/network/interfaces.new;
-echo "         bridge-fd 0" >> /etc/network/interfaces.new;
+echo auto ${IntNum} >> FiileInt;
+echo iface ${IntNum} inet manual >> FiileInt;
+echo -e '\t'bridge-ports none >> FiileInt;
+echo -e '\t'bridge-stp off >> FiileInt;
+echo -e '\t'bridge-fd 0 >> FiileInt;
 case $i in
   1)
-    echo \#ISP-HQ-R >> /etc/network/interfaces.new;
+    echo \#ISP-HQ-R >> FiileInt;
     ;;
   2)
-    echo \#ISP-BR-R >> /etc/network/interfaces.new;
+    echo \#ISP-BR-R >> FiileInt;
     ;;
   3)
-    echo \#HQ-R-HQ-SRV >> /etc/network/interfaces.new;
+    echo \#HQ-R-HQ-SRV >> FiileInt;
     ;;
   4)
-    echo \#BR-R-BR-SRV >> /etc/network/interfaces.new;
+    echo \#BR-R-BR-SRV >> FiileInt;
     ;;
 esac
+    echo " " >> FiileInt;
 done
 done
