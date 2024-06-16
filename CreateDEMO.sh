50a705811e09c4da1d816431a5474bcac6c57b5e
GrayNskDEMO <161566997+GrayNskDEMO@users.noreply.github.com>
2024-06-16 16:49:20 +0700

    Update CreateDEMO.sh

--- CreateDEMO.sh
+++ CreateDEMO.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 cat ./BridgeInterfaces.txt >> /etc/network/interfaces;
+#Создание интерфейса для машины ISP0 -доступ в интернет для всех стендов
+cat ./BridgeInterfaces.txt >> /etc/network/interfaces;
+echo auto vmbr1 >> /etc/network/interfaces;
+echo iface vmbr11 inet manual >> /etc/network/interfaces;
+echo         bridge-ports none >> /etc/network/interfaces;
+echo         bridge-stp off >> /etc/network/interfaces;
+echo         bridge-fd 0 >> /etc/network/interfaces;
+echo #ISP0 >> /etc/network/interfaces;
+#Создание интерфейсов для виртуальных машин
+for s in $(seq 1 10); do
+for i in $(seq 1 4); do
+cat ./BridgeInterfaces.txt >> /etc/network/interfaces;
+echo auto vmbr11
+iface vmbr11 inet manual
+        bridge-ports none
+        bridge-stp off
+        bridge-fd 0
+#ISP-HQ-R
+done
+done
 mkdir /etc/net/ifaces/vmbr1 ; cp ./vmbropt.txt /etc/net/ifaces/vmbr1/options
 for s in $(seq 1 10); do
 for i in $(seq 1 4); do
