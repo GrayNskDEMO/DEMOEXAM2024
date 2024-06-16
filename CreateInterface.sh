92de9cd3f68b6d42746b4241d6df2c11f90112e7
GrayNskDEMO <161566997+GrayNskDEMO@users.noreply.github.com>
2024-06-16 18:17:11 +0700

    Update CreateInterface.sh

--- CreateInterface.sh
+++ CreateInterface.sh
@@ -2,33 +2,33 @@
 #Создание интерфейса для машины ISP0 -доступ в интернет для всех стендов
 #cat ./BridgeInterfaces.txt >> /etc/network/interfaces.new;
 FileInt=/etc/network/interfaces.new1
-echo -e '\n'auto vmbr1 >> FiileInt;
-echo iface vmbr1 inet manual >> FileInt;
-echo -e '\t'bridge-ports none >> FileInt;
-echo -e '\t'bridge-stp off >> FileInt;
-echo -e '\t'bridge-fd 0 >> FileInt;
-echo \#ISP0 >> FileInt;
+echo -e '\n'auto vmbr1 >> $FiileInt;
+echo iface vmbr1 inet manual >> $FiileInt;
+echo -e '\t'bridge-ports none >> $FiileInt;
+echo -e '\t'bridge-stp off >> $FiileInt;
+echo -e '\t'bridge-fd 0 >> $FiileInt;
+echo \#ISP0 >> $FiileInt;
 #Создание интерфейсов для виртуальных машин
 for s in $(seq 1 20); do
 for i in $(seq 1 4); do
 IntNum=vmbr$s$i
-echo -e '\n'auto ${IntNum} >> FileInt;
-echo iface ${IntNum} inet manual >> FileInt;
-echo -e '\t'bridge-ports none >> FileInt;
-echo -e '\t'bridge-stp off >> FileInt;
-echo -e '\t'bridge-fd 0 >> FileInt;
+echo -e '\n'auto ${IntNum} >> $FiileInt;
+echo iface ${IntNum} inet manual >> $FiileInt;
+echo -e '\t'bridge-ports none >> $FiileInt;
+echo -e '\t'bridge-stp off >> $FiileInt;
+echo -e '\t'bridge-fd 0 >> $FiileInt;
 case $i in
   1)
-    echo \#ISP-HQ-R >> FileInt;
+    echo \#ISP-HQ-R >> $FiileInt;
     ;;
   2)
-    echo \#ISP-BR-R >> FileInt;
+    echo \#ISP-BR-R >> $FiileInt;
     ;;
   3)
-    echo \#HQ-R-HQ-SRV >> FileInt;
+    echo \#HQ-R-HQ-SRV >> $FiileInt;
     ;;
   4)
-    echo \#BR-R-BR-SRV >> FileInt;
+    echo \#BR-R-BR-SRV >> $FiileInt;
     ;;
 esac
 done
